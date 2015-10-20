0bf6c44c6a98664fa02e6508eb4cd50354f1ec56
DhammaWays <lekhraj@gmail.com>
2015-10-20 20:55:28 +0530

    Learn your own algorithms: k-neighbor, random forest and adaboost

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,11 +31,30 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+def getClassificationAccuracy( clf ):
+    #from sklearn import tree
+    from sklearn.metrics import accuracy_score    
+    #clf = tree.DecisionTreeClassifier( **kwargs )
+    t0 = time()
+    clf = clf.fit(features_train, labels_train)
+    print "Training time:", round(time()-t0, 3), "s", "Sample Size, Features:", len(features_train), len(features_train[0])
+    
+    t0 = time()
+    pred = clf.predict( features_test ) 
+    print "Prediction Time time:", round(time()-t0, 3), "s", "Sample Size, Features:", len(features_test), len(features_test[0])
+    
+    return accuracy_score( labels_test, pred )
 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
 
+clf = KNeighborsClassifier(n_neighbors=15, weights='distance') # Accuracy = 0.94
+#clf = RandomForestClassifier(n_estimators=20, min_samples_split=15, max_features="log2") # accuracy = 0.92
+#clf = AdaBoostClassifier(n_estimators=20, learning_rate=2.0) # accuracy = 0.936
 
-
-
+accuracy = getClassificationAccuracy(clf)
+print "Accuracy:", accuracy
 
 
 try:
