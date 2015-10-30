f9e2811c898fa50d60f84e21e3001c92bcfd1fb6
DhammaWays <lekhraj@gmail.com>
2015-10-30 19:08:10 +0530

    Updated after final lesson 15: Summary

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,23 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+# split into training and test data
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 
+
+### it's all yours from here forward!  
+
+### your code goes here ###
+def getDecTreeAccuracy( **kwargs ):
+    from sklearn import tree
+    from sklearn.metrics import accuracy_score, precision_score, recall_score    
+    clf = tree.DecisionTreeClassifier( **kwargs )
+    clf = clf.fit(features_train, labels_train)
+    pred = clf.predict( features_test ) 
+    print "Test Data: Predicted number of person of interest ", len(pred[pred == 1.0]), "out of total persons", len(pred)
+    return [accuracy_score(labels_test, pred), precision_score(labels_test, pred), recall_score(labels_test, pred)]
+
+#accuracy = getDecTreeAccuracy( min_samples_split=50 )
+accuracy = getDecTreeAccuracy()
+print "Accuracy, Precision, Recall scores:", accuracy
--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -25,8 +25,24 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+# split into training and test data
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 
 
 ### it's all yours from here forward!  
+
+### your code goes here ###
+def getDecTreeAccuracy( **kwargs ):
+    from sklearn import tree
+    from sklearn.metrics import accuracy_score    
+    clf = tree.DecisionTreeClassifier( **kwargs )
+    clf = clf.fit(features_train, labels_train)
+    pred = clf.predict( features_test ) 
+    return accuracy_score( labels_test, pred )
+
+#accuracy = getDecTreeAccuracy( min_samples_split=50 )
+accuracy = getDecTreeAccuracy()
+print accuracy
 
 
